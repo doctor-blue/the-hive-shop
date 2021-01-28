77d3d8ba253387df0d51d4f3ef3b6a27e7246ce6
hoangvu2012 <hn.minhvu@gmail.com>
2021-01-28 18:36:02 +0700

    revert

--- android-app/app/src/main/java/com/doctorblue/thehiveshop/api/HiveService.kt
+++ android-app/app/src/main/java/com/doctorblue/thehiveshop/api/HiveService.kt
@@ -3,15 +3,16 @@ package com.doctorblue.thehiveshop.api
 import com.doctorblue.thehiveshop.model.User
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface HiveService {
 
     @POST("/authentication/signup")
-    suspend fun signUp(user: User)
+    suspend fun signUp(@Body user: User)
 
     @POST("/authentication/signin")
-    suspend fun signIn(user: User) : User
+    suspend fun signIn(@Body user: User) : User
 
     companion object {
         private const val BASE_URL = "http://192.168.15.105:8080"
