f1f4fb02eeb7a71e246122f48cf3446be0fa02fd
John Smith <doctorblue.king@gmail.com>
2021-02-01 20:05:19 +0700

    Update code base

---
+++ android-app/app/src/main/java/com/doctorblue/thehiveshop/ui/ProductFragment.kt
@@ -0,0 +1,19 @@
+package com.doctorblue.thehiveshop.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProductFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
